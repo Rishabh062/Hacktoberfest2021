e0142ebc00b79fc5964c0b537082ed52bc18d8f8
E S Gayatri <gayatri.es20@gmail.com>
2021-10-01 00:40:33 +0530

    duplicate element in array added

---
+++ C++/duplicate_element_in_array.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cout<<"Enter total testcases: "<<endl;
+    cin>>test;
+    while(test--)
+    {
+        int n;
+        cout<<"Enter total: "<<endl;
+        cin>>n;
+        vector<int> v;
+        for(int i=0;i<n;i++)
+        {
+            int temp;
+            cin>>temp;
+            v.push_back(temp);
+        }
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[v[i]]++;
+        }
+        for(auto itr: mp)
+        {
+            if(itr.second>1)
+            {
+                cout<<"The duplicate element is: "<<itr.first<<endl;
+            }
+        }
+    }
+    return 0;
+}
