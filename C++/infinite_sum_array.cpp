434591f0ce64add9f67d960f0cc8b3073756a0a8
E S Gayatri <70585276+gayatri517@users.noreply.github.com>
2021-10-01 12:39:56 +0530

    problem statement added

--- C++/infinite_sum_array.cpp
+++ C++/infinite_sum_array.cpp
@@ -1,3 +1,10 @@
+/* Problem Statement: Given an array “A” of N integers and you have also defined the new array “B” as a concatenation of array “A” for an infinite number of times.
+For example, if the given array “A” is [1,2,3] then, infinite array “B” is [1,2,3,1,2,3,1,2,3,.......].
+Now you are given Q queries, 
+each query consists of two integers “L“ and “R”. Your task is to find the sum of the subarray from index “L” to “R” (both inclusive) in the infinite array “B” for each query.
+Time Complexity: O(nlogn)
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
