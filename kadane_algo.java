1185780e2d52d5b0016285034a8dd6334c84791c
Raghav Rastogi <51928619+raghav2404@users.noreply.github.com>
2021-10-01 23:33:23 +0530

    Update and rename Java/KadaneAlgo.java to kadane_algo.java

--- kadane_algo.java
+++ kadane_algo.java
@@ -1,3 +1,10 @@
+#Ques:Write an efficient program to find the sum of contiguous subarray within a one-dimensional array of numbers that has the largest sum. 
+#Solution:Explanation: 
+The simple idea of Kadane’s algorithm is to look for all positive contiguous segments of the array (max_ending_here is used for this). 
+    And keep track of maximum sum contiguous segment among all positive segments (max_so_far is used for this). 
+    Each time we get a positive-sum compare it with max_so_far and update max_so_far if it is greater than max_so_far .
+#Time Complexity: O(N)
+
 class Kadane
 {
     public static void main (String[] args)
