d1cb4f2fcb57b8faea769bf70274aa387620e1b1
Aayush Varshney <61947214+aayushvarshney@users.noreply.github.com>
2021-10-01 20:50:41 +0530

    Update LongestCommonSubsequence.cpp

--- C++/LongestCommonSubsequence.cpp
+++ C++/LongestCommonSubsequence.cpp
@@ -1,5 +1,6 @@
 /*
-Given two sequences, find the length of longest subsequence present in both of them. A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous. For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”.
+Given two sequences, find the length of longest subsequence present in both of them. A subsequence is a sequence that appears in the same relative order, 
+but not necessarily contiguous.
 Time complexity : O(N*M) where N and M are size of strings.
 */
 #include<bits/stdc++.h>
