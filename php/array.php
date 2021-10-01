1b11b76283ccd3b6bab2b5615697f8be776f2100
Neila126 <88262494+Neila126@users.noreply.github.com>
2021-10-01 09:32:20 +0530

    Create array.php

---
+++ php/array.php
@@ -0,0 +1,25 @@
+<html>
+   <body>
+   
+      <?php
+         /* First method to create array. */
+         $numbers = array( 1, 2, 3, 4, 5);
+         
+         foreach( $numbers as $value ) {
+            echo "Value is $value <br />";
+         }
+         
+         /* Second method to create array. */
+         $numbers[0] = "one";
+         $numbers[1] = "two";
+         $numbers[2] = "three";
+         $numbers[3] = "four";
+         $numbers[4] = "five";
+         
+         foreach( $numbers as $value ) {
+            echo "Value is $value <br />";
+         }
+      ?>
+      
+   </body>
+</html>
