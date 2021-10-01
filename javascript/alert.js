0e25c1731831443c58266f588ff9e93b9333e26c
Rishav Jaiswal <rishavjaiss14@gmail.com>
2021-10-01 23:19:16 +0530

    alert.js

---
+++ javascript/alert.js
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <script>
+      function main() {
+        alert('You Clicked me!');
+      }
+    </script>
+  </head>
+  <body>
+    <button id="click" onclick="main()">Click me!</button>
+  </body>
+</html>
