a414946064eeffda823be083c943aea4804ad5fd
E S Gayatri <70585276+gayatri517@users.noreply.github.com>
2021-10-01 12:33:30 +0530

    problem statement added

--- C++/beautiful_string.cpp
+++ C++/beautiful_string.cpp
@@ -1,3 +1,11 @@
+/* Problem Statement: Ninja has been given a binary string ‘STR’ containing either ‘0’ or ‘1’. A binary string is called beautiful if it contains alternating 0s and 1s.
+For Example:‘0101’, ‘1010’, ‘101’, ‘010’ are beautiful strings.
+He wants to make ‘STR’ beautiful by performing some operations on it. In one operation, Ninja can convert ‘0’ into ‘1’ or vice versa.
+Your task is to determine the minimum number of operations Ninja should perform to make ‘STR’ beautiful.
+
+Time Complexity: O(n) 
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
