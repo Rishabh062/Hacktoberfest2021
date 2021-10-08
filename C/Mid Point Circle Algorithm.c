7af1ca1dcd57ddb72e0ca680c73f6ed0c4463f0f
Devashish Pant <83301269+devashishpant89@users.noreply.github.com>
2021-10-08 08:22:13 +0530

    Mid Point Circle Algorithm
    Mid Point Circle Algorithm in C hacktoberfest 2021

---
+++ C/Mid Point Circle Algorithm.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<graphics.h>
+ 
+void drawcircle(int x0, int y0, int radius)
+{
+    int x = radius;
+    int y = 0;
+    int err = 0;
+ 
+    while (x >= y)
+    {
+putpixel(x0 + x, y0 + y, 7);
+putpixel(x0 + y, y0 + x, 7);
+putpixel(x0 - y, y0 + x, 7);
+putpixel(x0 - x, y0 + y, 7);
+putpixel(x0 - x, y0 - y, 7);
+putpixel(x0 - y, y0 - x, 7);
+putpixel(x0 + y, y0 - x, 7);
+putpixel(x0 + x, y0 - y, 7);
+ 
+if (err <= 0)
+{
+    y += 1;
+    err += 2*y + 1;
+}
+ 
+if (err > 0)
+{
+    x -= 1;
+    err -= 2*x + 1;
+}
+    }
+}
+ 
+int main()
+{
+int gdriver=DETECT, gmode, error, x, y, r;
+initgraph(&gdriver, &gmode, "c:\\turboc3\\bgi");
+ 
+printf("Enter radius of circle: ");
+scanf("%d", &r);
+ 
+printf("Enter co-ordinates of center(x and y): ");
+scanf("%d%d", &x, &y);
+drawcircle(x, y, r);
+ 
+return 0;
+}
