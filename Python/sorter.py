1013b5811833fce582557d00eb017c55929db8f4
tez-580 <92677016+tez-580@users.noreply.github.com>
2021-10-19 10:35:15 +0530

    Create sorter.py

---
+++ Python/sorter.py
@@ -0,0 +1,21 @@
+#This function can be used to sort list of integers, floating point number, string and others.
+numbers = [1, 3, 4, 2]
+
+numbers.sort()
+  
+print(numbers)
+  
+
+decimalnumber = [2.01, 2.00, 3.67, 3.28, 1.68]
+
+decimalnumber.sort()
+  
+print(decimalnumber)
+  
+
+words = ["Geeks", "For", "Geeks"]
+  
+
+words.sort()
+  
+print
