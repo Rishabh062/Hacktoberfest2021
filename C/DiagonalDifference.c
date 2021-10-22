ab703175ec7774e37d4cdddde7a4de0d769a61c4
Pendyala Sri Harshita <71591775+PendyalaHarshita@users.noreply.github.com>
2021-10-22 11:46:41 +0530

    Update DiagonalDifference.c

--- C/DiagonalDifference.c
+++ C/DiagonalDifference.c
@@ -1,3 +1,6 @@
+//Program to find the difference of the diagonals of a matrix
+//Time complexity : O(n)
+
 #include<stdio.h>
 #include<stdlib.h>
 int main()
@@ -5,12 +8,14 @@ int main()
   int a[10][10];
   int n,i,j;
   scanf("%d ",&n);
+  //Getting input
   for(i=0;i<n;i++)
   {
     for(j=0;j<n;j++)
     scanf("%d",&a[i][j]);
    }
   int sum1=0,sum2=0;
+  //Difference in the diagonals
  for(i=0;i<n;i++)
   {
     sum1=a[i][i]+sum1;
