cd08687ea28579969cceaea43d121c074d2e87e5
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2023-10-01 18:02:27 +0530

    Create SumOfSubarray.cpp

---
+++ C++/SumOfRespSubarray.cpp
@@ -0,0 +1,26 @@
+//This gives the sum of respective of subarrays
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int total = 0;
+
+    for (int i=0; i<n; i++){
+        int sum = 0;
+        for (int j=i; j<n; j++){
+            sum += arr[j];
+            cout<<sum<<endl;
+        }
+        total += sum;
+    }
+    cout<<"Total "<<total<<endl;
+    return 0;
+}
