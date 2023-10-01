14dc26294c3bd7b94f2d3a2d6d92e5badbf3aa1b
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2023-10-01 17:49:06 +0530

    Create Subarrays.cpp

---
+++ C++/Subarrays.cpp
@@ -0,0 +1,30 @@
+//This program give all the possible subarrays of an given array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << a[k]<<" ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
